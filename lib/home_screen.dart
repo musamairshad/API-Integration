9f334ac1accf62c46165ce026a1c0edfcf3b6e8f
Muhammad Usama <imusamairshad@gmail.com>
2023-08-06 17:00:17 +0500

    home_screen.dart is added.

---
+++ lib/home_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() {
+    return _HomeScreenState();
+  }
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('API Integration'),
+      ),
+      body: const Column(
+        children: [],
+      ),
+    );
+  }
+}
