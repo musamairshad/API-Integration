2a5db5b8797708a3e2c20ec634d96d6aecc32778
Muhammad Usama <imusamairshad@gmail.com>
2023-08-25 23:28:49 +0500

    main.dart is modified.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'views/home_screen.dart';
+import 'package:api_integration/views/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomeScreen(),
+      home: HomeScreen(),
     );
   }
 }
