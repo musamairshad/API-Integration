1ada3d7e744f7e7412d6812ed6b080626b181b6f
Muhammad Usama <imusamairshad@gmail.com>
2023-09-02 00:03:23 +0500

    home_screen.dart is modified.

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -1,19 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:api_integration/views/products_screen.dart';
+import 'package:api_integration/views/auth_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Center(
-          child: Text('API Integration'),
-        ),
-      ),
-      body: const ProductsScreen(),
+    return const Scaffold(
+      body: AuthScreen(),
     );
   }
 }
