69f529d3e8a947856a6f7b3aa650a08deadc22b4
Muhammad Usama <imusamairshad@gmail.com>
2023-09-01 17:26:09 +0500

    post.dart is added.

--- lib/models/post.dart
+++ lib/models/post.dart
@@ -1,38 +1,24 @@
 class Post {
-  Post({
-      int? userId, 
-      int? id, 
-      String? title, 
-      String? body,}){
-    _userId = userId;
-    _id = id;
-    _title = title;
-    _body = body;
-}
+  int? userId;
+  int? id;
+  String? title;
+  String? body;
 
-  Post.fromJson(dynamic json) {
-    _userId = json['userId'];
-    _id = json['id'];
-    _title = json['title'];
-    _body = json['body'];
+  Post({this.userId, this.id, this.title, this.body});
+
+  Post.fromJson(Map<String, dynamic> json) {
+    userId = json['userId'];
+    id = json['id'];
+    title = json['title'];
+    body = json['body'];
   }
-  int? _userId;
-  int? _id;
-  String? _title;
-  String? _body;
-
-  int? get userId => _userId;
-  int? get id => _id;
-  String? get title => _title;
-  String? get body => _body;
 
   Map<String, dynamic> toJson() {
-    final map = <String, dynamic>{};
-    map['userId'] = _userId;
-    map['id'] = _id;
-    map['title'] = _title;
-    map['body'] = _body;
-    return map;
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['userId'] = userId;
+    data['id'] = id;
+    data['title'] = title;
+    data['body'] = body;
+    return data;
   }
-
 }
