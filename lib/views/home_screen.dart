86352d0ec09d165b4e6c55bc0134e97e406f5a9c
Muhammad Usama <imusamairshad@gmail.com>
2023-09-04 23:16:46 +0500

    home_screen.dart is modified.

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:api_integration/views/auth_screen.dart';
+import 'package:api_integration/views/upload_image_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -7,7 +7,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: AuthScreen(),
+      body: UploadImageScreen(),
     );
   }
 }
